45362a0a283716e0c1349d3c1dadfe1de267193e
chris4824 <chris482422@gmail.com>
2015-04-20 21:13:25 -0400

    Validus bringup

--- validus.mk
+++ validus.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/jfltecan/full_jfltecan.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltevl \
@@ -12,5 +12,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.4.2/KOT49H/I337MVLUFNC1:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltevl-user 4.4.2 KOT49H I337MVLUFNC1 release-keys"
 
-PRODUCT_NAME := cm_jfltecan
+PRODUCT_NAME := validus_jfltecan
 PRODUCT_DEVICE := jfltecan
